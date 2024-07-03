ad68174b3a3cd6747add8b6a8131b80f63e8f8f9
dat05 <datvtph50396@gmail.com>
2024-07-03 14:16:35 +0700

    Done menu

---
+++ src/raven/application/form/LoginForm.java
@@ -0,0 +1,124 @@
+package raven.application.form;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import javax.swing.JOptionPane;
+import net.miginfocom.swing.MigLayout;
+import raven.application.Application;
+import raven.toast.Notifications;
+
+/**
+ *
+ * @author Raven
+ */
+public class LoginForm extends javax.swing.JPanel {
+
+    public LoginForm() {
+        initComponents();
+        init();
+    }
+
+    private void init() {
+        setLayout(new MigLayout("al center center"));
+
+        lbTitle.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:$h1.font");
+
+        txtPass.putClientProperty(FlatClientProperties.STYLE, ""
+                + "showRevealButton:true;"
+                + "showCapsLock:true");
+        cmdLogin.putClientProperty(FlatClientProperties.STYLE, ""
+                + "borderWidth:0;"
+                + "focusWidth:0");
+        txtPass.setText("sa");
+        txtUser.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "User Name");
+        txtPass.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Password");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelLogin1 = new raven.application.form.PanelLogin();
+        lbTitle = new javax.swing.JLabel();
+        lbUser = new javax.swing.JLabel();
+        txtUser = new javax.swing.JTextField();
+        lbPass = new javax.swing.JLabel();
+        txtPass = new javax.swing.JPasswordField();
+        cmdLogin = new javax.swing.JButton();
+
+        lbTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbTitle.setText("Login");
+        panelLogin1.add(lbTitle);
+
+        lbUser.setText("User Name");
+        panelLogin1.add(lbUser);
+
+        txtUser.setText("sa");
+        txtUser.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUserActionPerformed(evt);
+            }
+        });
+        panelLogin1.add(txtUser);
+
+        lbPass.setText("Password");
+        panelLogin1.add(lbPass);
+
+        txtPass.setText("sa");
+        panelLogin1.add(txtPass);
+
+        cmdLogin.setText("Login");
+        cmdLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdLoginActionPerformed(evt);
+            }
+        });
+        panelLogin1.add(cmdLogin);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(218, Short.MAX_VALUE)
+                .addComponent(panelLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(197, 197, 197))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(68, 68, 68)
+                .addComponent(panelLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(96, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void cmdLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdLoginActionPerformed
+        if (txtUser.getText().isEmpty()) {
+            Notifications.getInstance().show(Notifications.Type.WARNING, Notifications.Location.TOP_CENTER, "Không nhập gì à :)?!");
+        } else if (txtUser.getText().equalsIgnoreCase("sa")) {
+            if (txtPass.getText().equalsIgnoreCase("sa")) {
+                Application.login();
+                Notifications.getInstance().show(Notifications.Type.SUCCESS, Notifications.Location.TOP_CENTER, "Hoan hô giỏi quá. Đăng nhập thành công!");
+            } else {
+                Notifications.getInstance().show(Notifications.Type.WARNING, Notifications.Location.TOP_CENTER, "Sai tài khoản hoặc mật khẩu!");
+            }
+
+        }
+
+    }//GEN-LAST:event_cmdLoginActionPerformed
+
+    private void txtUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUserActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtUserActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdLogin;
+    private javax.swing.JLabel lbPass;
+    private javax.swing.JLabel lbTitle;
+    private javax.swing.JLabel lbUser;
+    private raven.application.form.PanelLogin panelLogin1;
+    private javax.swing.JPasswordField txtPass;
+    private javax.swing.JTextField txtUser;
+    // End of variables declaration//GEN-END:variables
+}
