bc873cb77ca4f76a6a23a5c7061e5f6a09e4e347
nvv <vunvph49243@gmail.com>
2024-07-29 18:01:56 +0700

    Meger

--- src/raven/application/Application.java
+++ src/raven/application/Application.java
@@ -80,7 +80,6 @@ public class Application extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
